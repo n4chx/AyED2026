1e6023e8a560c14e6892b70e2027578f25ebb990
n4chx <juani-rodriguez10@hotmail.com>
2026-05-01 11:47:34 -0300

    finish

--- 2026_practico4_ayed_ignacio_rodriguez.py
+++ 2026_practico4_ayed_ignacio_rodriguez.py
@@ -97,3 +97,65 @@ while True:
             contador_pares += 1
     except ValueError:
         print("Entrada no valida. Ingrese un numero entero o 'q' para salir.")
+print("Suma de numeros pares ingresados:", suma_pares)
+if contador_pares > 0:
+    promedio_pares = suma_pares / contador_pares
+    print("Promedio de numeros pares ingresados:", promedio_pares)
+
+#########################################
+# Ejercicio 9 : Implementar un programa que permita ingresar un valor al usuario, llamado n y muestre en pantalla los numeros empezando en 1, hasta n pero en saltos de 3. n debe ser al menos 7
+#########################################
+n = int(input("Ingrese un valor (al menos 7): "))
+while n < 7:
+    n = int(input("Valor no valido. Ingrese un valor superior a 7: "))
+i = 1
+while i <= n:
+    print(i)
+    i += 3
+
+#########################################
+# Ejercicio 10 : Usar bucles anidados para realizar un programa que imprima en pantalla lo siguiente, seran 9 filas con 9 columnas de digitos, primera fila 1, segunda fila 2, tercera fila 3, etc. hasta la novena fila con el numero 9
+#########################################
+for i in range(1, 10):
+    for j in range(1, 10):
+        print(i, end=" ")
+    print()
+
+#########################################
+# Ejercicio 11 : Dada tres variables a, b y c, implementar un programa que muestre todas las permutaciones posibles, empezando en 000 y terminando en 999
+#########################################
+for a in range(10):
+    for b in range(10):
+        for c in range(10):
+            print(f"{a}{b}{c}")
+
+#########################################
+# Ejercicio 12 : Pedir un numero al usuario que deberia estar entre 1 y 20
+#########################################
+n = int(input("Ingrese un numero entre 1 y 20: "))
+while n < 1 or n > 20:
+    n = int(input("Valor no valido. Ingrese un numero entre 1 y 20: "))
+print("Ingreso correcto. El numero ingresado es:", n)
+
+#########################################
+# Ejercicio 13 : Imprimir en pantalla todos los numeros enteros entre 1 y un numero n ingresado por el usuario
+#########################################
+n = int(input("Ingrese un numero entero: "))
+i = 1
+while i <= n:
+    print(i)
+    i += 1
+
+#########################################
+# Ejercicio 14 : Imprimir en pantalla el siguiente esquema, segun un numero n ingresado por el usuario. 
+#########################################
+n = int(input("Ingrese un numero entero: "))
+i = n
+while i >= 1:
+    j = i
+    while j >= 1:
+        print(j, end=" ")
+        j -= 1
+    print()
+    i -= 1
+
